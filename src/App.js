8da009c6d6efa05a3899ee1ddb0509fdd9db2a07
aamirjawed <aamirjawed682@gmail.com>
2024-10-14 15:03:24 +0530

    "Added redux-devtools-extension and redux-logger,  modified App.js, cakeActions.js, cakeReducer.js, and store.js"

--- src/App.js
+++ src/App.js
@@ -6,14 +6,23 @@ import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import CounterContainer from './components/CounterContainer';
 
+import ItemContainer from './components/ItemContainer';
+
+
+
+
 function App() {
   return (
     <Provider store={store}>
       <div className='style'>
+      <ItemContainer cake />
+      <ItemContainer />
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
         <CounterContainer />
+        
+       
       </div>
     </Provider>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootreducer from "./rootReducer";
 
 
 
-const store = createStore(rootreducer)
+
+const store = createStore(rootreducer, composeWithDevTools(
+    applyMiddleware(logger)
+))
 
 export default store
